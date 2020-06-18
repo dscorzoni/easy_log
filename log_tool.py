2a5b331482f1265424b77b60fb41c2cd1f0acc70
Danilo Scorzoni Re <danilo.scorzoni@gmail.com>
2020-06-18 18:11:45 -0400

    Added basic form for logging event.

--- log_tool.py
+++ log_tool.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # App instance
 app = Flask(__name__)
@@ -7,5 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("base.html")
+
+@app.route('/log_form/')
+def log_form():
+    return render_template("log_form.html")
+
+@app.route('/log_action', methods=['POST'])
+def log_action():
+    log_string = request.form['log_string']
+    return 'Log string is: ' + log_string
 
 app.run()
