afd15695c27ac8a1a0d0703b05c0c4c92a881116
Danilo Scorzoni Re <danilo.scorzoni@gmail.com>
2020-06-18 20:49:27 -0400

    Adding log to SQLite db.

--- log_tool.py
+++ log_tool.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 # App instance
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///log_db.db'
+
+
+
+
+# Data Model
+db = SQLAlchemy(app)
+
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    log_string = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default = datetime.now())
+
+
+
 
 # Routes
 @app.route('/')
@@ -15,6 +33,13 @@ def log_form():
 @app.route('/log_action', methods=['POST'])
 def log_action():
     log_string = request.form['log_string']
-    return 'Log string is: ' + log_string
 
-app.run()
+    # Adding to the database
+    row = Log(log_string = log_string)
+    db.session.add(row)
+    db.session.commit()
+
+    return 'Added to database: ' + log_string
+
+if __name__ == '__main__':
+    app.run(debug=True)
